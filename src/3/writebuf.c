87001a21301d81b810671492ffa25be9028af7da
Faraway <hzylovelyl@gmail.com>
2018-12-18 01:29:15 +0800

    ok

--- src/3/writebuf.c
+++ src/3/writebuf.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+
 #define BUF_SIZE 1024
+#define N 8
+
 int main()
 {
     int shmid = shmget(IPC_PRIVATE, BUF_SIZE, IPC_CREAT | 0666);
+    int *addr = shmat(shmid, NULL, 0);
 }
