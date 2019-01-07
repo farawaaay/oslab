ef81f983206fbf8096e5e63d3eef2d0307995b72
Faraway <hzylovelyl@gmail.com>
2019-01-08 01:22:54 +0800

    ubuntu update!

--- src/3/readbuf.c
+++ src/3/readbuf.c
@@ -4,6 +4,8 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 
+#define MEM 75
+#define SEM_KEY 76
 #define BUF_SIZE 32
 #define N 8
 
@@ -32,32 +34,22 @@ void V(int semid, int index)
     semop(semid, &sem, 1);
     return;
 }
-// 设缓冲区大小为N，队头out，队尾in，out、in均是下标表示:
 
-// 初始时，in=out=0
-// 队头队尾的更新用取模操作，out=(out+1)%N，in=(in+1)%N
-// out==in表示缓冲区空，(in+1)%N==out表示缓冲区满
-// 入队que[in]=value;in=(in+1)%N;
-// 出队ret =que[out];out=(out+1)%N;
-// 数据长度 len =( in - out + N) % N
 
 Mem *m;
 
 int main()
 {
-    int shmid = shmget(IPC_PRIVATE, sizeof(Mem), IPC_CREAT | 0666);
+    int shmid = shmget(MEM, sizeof(Mem), IPC_CREAT | 0666);
     m = (Mem *)shmat(shmid, NULL, 0);
-    int semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
-    printf("%d\n", semid);
+    int semid = semget(SEM_KEY, 2, IPC_CREAT | 0666);
     FILE *fp = fopen("./input.txt", "r");
 
     int i = 0;
     size_t total = -1;
     while (1)
     {
-        printf("P\n");
         P(semid, 0);
-        printf("P\n");
         if (total == -1)
         {
             fseek(fp, 0L, SEEK_END);
@@ -75,7 +67,6 @@ int main()
             fread(m->buf[i = (i + 1) % N], BUF_SIZE, 1, fp);
             total -= BUF_SIZE;
         }
-        printf("V\n");
         V(semid, 1);
 
         if (total == 0)
--- src/3/writebuf.c
+++ src/3/writebuf.c
@@ -4,6 +4,8 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 
+#define MEM 75
+#define SEM_KEY 76
 #define BUF_SIZE 32
 #define N 8
 
@@ -32,27 +34,18 @@ void V(int semid, int index)
     semop(semid, &sem, 1);
     return;
 }
-// 设缓冲区大小为N，队头out，队尾in，out、in均是下标表示:
-
-// 初始时，in=out=0
-// 队头队尾的更新用取模操作，out=(out+1)%N，in=(in+1)%N
-// out==in表示缓冲区空，(in+1)%N==out表示缓冲区满
-// 入队que[in]=value;in=(in+1)%N;
-// 出队ret =que[out];out=(out+1)%N;
-// 数据长度 len =( in - out + N) % N
 
 Mem *m;
 
 int main()
 {
-    int shmid = shmget(IPC_PRIVATE, sizeof(Mem), IPC_CREAT | 0666);
+    int shmid = shmget(MEM, sizeof(Mem), IPC_CREAT | 0666);
     m = (Mem *)shmat(shmid, NULL, 0);
-    int semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+    int semid = semget(SEM_KEY, 2, IPC_CREAT | 0666);
     FILE *fp = fopen("./output.txt", "w");
 
     int i = 0;
     size_t total = -1;
-    printf("exec write\n");
     while (1)
     {
         P(semid, 1);
